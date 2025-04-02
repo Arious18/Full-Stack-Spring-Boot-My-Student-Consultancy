be395584ad74c99d337465afdd87c6ae58e74674
Azat Vepakulyyev <azatvepakulyyev@gmail.com>
2025-04-02 13:43:55 +0300

    all files  commit  to github just for test

--- backend/src/main/java/tmtalyp/backend/BackendApplication.java
+++ backend/src/main/java/tmtalyp/backend/BackendApplication.java
@@ -1,8 +1,8 @@
 package tmtalyp.backend;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class BackendApplication {
 
