c195b5b03602198198748b2722ca555b6ff203ef
Azat Vepakulyyev <126404974+Arious18@users.noreply.github.com>
2025-05-06 15:50:21 +0300

    Update StorageService.java

--- backend/src/main/java/tmtalyp/backend/universities/StorageService.java
+++ backend/src/main/java/tmtalyp/backend/universities/StorageService.java
@@ -18,7 +18,7 @@ public class StorageService {
 
     private final S3Client s3Client;
     private final String bucketName = "my-data";
-    private final String baseUrl = "https://pub-cab830fe342c4f9480be11e8b3347409.r2.dev/";
+    private final String baseUrl = "Yoiur cloudflare key";
 
     @Autowired
     public StorageService(S3Client s3Client) {
